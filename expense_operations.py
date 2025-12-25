39171799a8fe3045063a30ec10b0cf806773d45f
Divya-SevanthyKumaran <maildivyask8@gmail.com>
2025-12-25 14:29:50 +0530

    Add add_expense function

--- expense_operations.py
+++ expense_operations.py
@@ -1,1 +1,18 @@
+valid_categories = ["Food", "Travel", "Medical", "Rent", "Other"]
 
+def add_expense(expenses, date, category, amount, description):
+    if category not in valid_categories:
+        return "Enter valid category"
+
+    if amount <= 0:
+        return "The amount should be above 0"
+
+    expense = {
+        "Date": date,
+        "Category": category,
+        "Amount": amount,
+        "Description": description
+    }
+
+    expenses.append(expense)
+    return "Expense added successfully"
